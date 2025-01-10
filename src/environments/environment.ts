b564f8db23e3bc89af63298fc4a51d0ebd732aca
GaneshBirajdar1399 <ganeshbirajdar321@gmail.com>
2025-01-10 15:55:20 +0530

    add proxy.conf.js

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  apiUrlUsers: 'http://localhost:3000/users',
+  apiUrlCart: 'http://localhost:3000/cart',
+  compareUrl: 'http://localhost:3000/compare',
+  apiUrlProducts: 'http://localhost:3000/products',
+  webDataUrl: 'http://localhost:3000/otherinfo',
+  apiUrl: 'http://localhost:3000',
+  apiUrlOrders: 'http://localhost:3000/orders',
+  wishlistUrl: 'http://localhost:3000/wishlist',
+};
